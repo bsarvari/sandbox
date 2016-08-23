a5c0231962d19e26e156b0d91e5ec8544cae4ec5
Balazs Sarvari <balazs.sarvari@gmail.com>
2016-08-23 15:25:43 +0200

    Renaming a variable

--- programming-excercises/interview-cake/03-highest-product-of-3-ints/03-highest-product-of-3-ints.js
+++ programming-excercises/interview-cake/03-highest-product-of-3-ints/03-highest-product-of-3-ints.js
@@ -67,24 +67,24 @@ function getHighestProductOfThreeInts(ints){
       gatherSmallestNegatives();
     }
   }
-  var c = []; // contributors
-  if(l1 !== undefined) c.push(l1);
-  if(l2 !== undefined) c.push(l2);
-  if(l3 !== undefined) c.push(l3);
-  if(s3 !== undefined) c.push(s3);
-  if(s2 !== undefined) c.push(s2);
-  if(s1 !== undefined) c.push(s1);
+  var factors = []; // contributors
+  if(l1 !== undefined) factors.push(l1);
+  if(l2 !== undefined) factors.push(l2);
+  if(l3 !== undefined) factors.push(l3);
+  if(s3 !== undefined) factors.push(s3);
+  if(s2 !== undefined) factors.push(s2);
+  if(s1 !== undefined) factors.push(s1);
 
-  if(c.length == 3){
-    return c[0] * c[1] * c[2];
+  if(factors.length == 3){
+    return factors[0] * factors[1] * factors[2];
   } else {
     // making all possible combinations for a set of maximum 6 integers, where the size of the combination is 3.
     // (n 3) results in at most 20 combinations if n=6, and less if n<6, which is an affordable price
     var largest = -Infinity; // largest possible product of the three smallest negative and three largest positive or zero ints
-    for(var x=0; x< c.length; x++){
-      for(var y=x+1; y< c.length; y++){
-        for(var z=y+1; z< c.length; z++){
-          var candidate = c[x] * c[y] * c[z];
+    for(var x=0; x< factors.length; x++){
+      for(var y=x+1; y< factors.length; y++){
+        for(var z=y+1; z< factors.length; z++){
+          var candidate = factors[x] * factors[y] * factors[z];
           largest = largest > candidate ? largest : candidate;
         }
       }
